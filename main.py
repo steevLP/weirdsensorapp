767b5ae7cec1bc759423f710aee672c51ae12840
steev <mauricefl@outlook.de>
2023-05-21 22:42:36 +0200

    moved away from pytq for usage of matplotlib native ui + fixed various display issues and fetched proper values for graph

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import array
+from gettext import npgettext
 import os
 import datetime
 
@@ -6,10 +6,8 @@ import numpy
 import requests
 import sqlite3
 import csv
-import sys
-
-from PyQt6 import QtWidgets
-import pyqtgraph as pg
+import numpy as np
+import matplotlib.pyplot as plt
 
 # downloads all required data from target server
 def fetchData(year, month, day):
@@ -32,53 +30,68 @@ def fetchData(year, month, day):
             # read the first line of incoming data to determin if it is invalid or not
             if r.content.decode("utf-8").split("\n")[0] != empty:
                 # write data to file
-                open("./data/" + str(year).zfill(2) + "-" + str(month).zfill(2) + "-" + str(day).zfill(2) + "-3659.csv",
-                     'wb').write(r.content)
+                open("./data/" + str(year).zfill(2) + "-" + str(month).zfill(2) + "-" + str(day).zfill(2) + "-3659.csv", 'wb').write(r.content)
 
         # counts the self maintained date up a day and iterates month and year conditionally
         day = day + 1
-        if day >= 31:
-            month = month + 1
-            day = 1
-            print("add month")
-        if month >= 12:
+        # use modul to decide whether a month has 30 or 31 days
+        if month % 2 == 0:
+            if month == 2:
+                # catch edgecase februrary
+                if day >= 28:
+                    month = month + 1
+                    day = 1
+                    print("add month")
+            elif day >= 31:
+                month = month + 1
+                day = 1
+                print("add month")
+        elif month % 2 == 1:
+            if day >= 30:
+                month = month + 1
+                day = 1
+                print("add month")
+            
+        # check if december is through
+        if month == 12 and day > 31:
             year = year + 1
             month = 1
             day = 1
             print("add year")
 
+
+# function used to determin if a required file was already proccessed
+def isFileWritten(path, filename):
+    file = open(path + '/written.txt').read().splitlines()
+    for l in file:
+        if filename == str(l):
+            return True
+    return False
+
 # creates database and handles file storage
 def storeindb(c, db, path):
     directory = os.fsencode(path)
-
+    store = open(path + '/written.txt', 'a')
     for file in os.listdir(directory):
         filename = os.fsdecode(file)
         if filename.endswith(".csv"):
-            # print(os.path.join(directory, filename))
-            with open(path + "/" + filename, newline='') as csvfile:
-                data = csv.reader(csvfile, delimiter=';')
-                for row in data:
-                    print(row)
-                    if row[0] == "sensor_id":
-                        continue
+            written = isFileWritten(path, filename)
+            if not written:
+                store.write(filename + "\n")
+                with open(path + "/" + filename, newline='') as csvfile:
+                    data = csv.reader(csvfile, delimiter=';')
+                    for row in data:
+                        if row[0] == "sensor_id":
+                            continue
+                        else:
+                            todo = numpy.array(
+                                [row[0], row[1], row[2], row[3], row[4], row[5].split("T")[0], row[5].split("T")[0], row[6], row[9]])
+                            c.execute("INSERT INTO sen_data VALUES (?,?,?,?,?,?,?,?,?)", todo)
+                            db.commit()
                     else:
-                        todo = numpy.array([row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[9]])
-                        c.execute("INSERT INTO sen_data VALUES (?,?,?,?,?,?,?,?)",todo)
                         continue
-        else:
-            continue
-    db.commit()
-
-# function for the handling the main window
-class MainWindow(QtWidgets.QMainWindow):
-    def __init__(self, d1,d2, *args, **kwargs):
-        super(MainWindow, self).__init__(*args, **kwargs)
-        self.graphWidget = pg.PlotWidget()
-        self.setCentralWidget(self.graphWidget)
-
-        # plot data: x, y values
-        self.graphWidget.plot(d1)
-
+            else:
+                continue
 
 def main():
     # Software Configuration
@@ -88,42 +101,46 @@ def main():
     dbp = "app.db"
 
     # proccessed data
-    label = []
-    value = []
-
+    p1_averages = {}  # Dictionaries werden angelegt, um ein Datum und den jeweils zu dem Tag gehörenden Durchschnittswert zu speichern.
+    p2_averages = {}
+    discarded_values = 0  # Variable als anhaltspunkt wie viele Datensätze in der Verarbeitung verworfen werden.
+    
     # download data and store them for later use
     fetchData(year, month, day)
 
     # initialize sqlite database
-    # delete sqlitedb on each startup
-    if os.path.exists(dbp):
-        os.remove(dbp)
-
-    # create new database
     dbc = sqlite3.connect(dbp)
     c = dbc.cursor()
-    c.execute('''CREATE TABLE sen_data (sensor_id,sensor_type,location,lat,lon,timestamp,P1,P2);''')
-
+    c.execute('''CREATE TABLE IF NOT EXISTS sen_data (sensor_id,sensor_type,location,lat,lon,date,time,P1,P2);''')
     storeindb(c, dbc, "./data")
 
-    data = c.execute("SELECT P1 from sen_data;").fetchall()
+    data = c.execute("SELECT avg(P1), avg(P2), date(date) from sen_data group by date(date);").fetchall()
     dbc.close()
 
     # proccesses the results coming from the databse
     for i in data:
-        # filters all values that are higher then 100
-        if float(i[0]) < 100.0:
-            value += [float(i[0])]
-
-    # plt.plot(label, value)
-    # plt.show()
+        # filters all values that are higher then 150
+        if float(i[0]) < 150.0:
+            # add values to dictionary
+            p1_averages[i[2]] = i[0]
+            p2_averages[i[2]] = i[1]
 
     # setup gui and start it
-    app = QtWidgets.QApplication(sys.argv)
-    main = MainWindow(value, [])
-    main.show()
-    sys.exit(app.exec())
+    # Code zur Darstellung des Graphen, der die Durchschnittswerte eines Tages für je P1 und P2 Datensätze veranschaulicht
+    dates = list(p1_averages.keys())
+    p1_values = list(p1_averages.values())
+    p2_values = list(p2_averages.values())
 
+    print(p1_values)
+
+    plt.plot(dates, p1_values, label='PM 1.0')
+    plt.plot(dates, p2_values, label='PM 2.5')
+    plt.xlabel('Datum')
+    plt.ylabel('Tagesdurchschnitt in µg/m³')
+    plt.title('Messwerte des Staubsensors sds011_3659 im Jahr 2023')
+    plt.xticks(rotation=90)
+    plt.legend()
+    plt.show()
 
 if __name__ == '__main__':
     main()
